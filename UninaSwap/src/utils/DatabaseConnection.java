0f9d8b49b17d923647bb40454c00466c9d933ca1
Nunzi <Nunzi@Nunzia-Aiello>
2025-10-07 10:39:29 +0200

    database aggiornato

--- UninaSwap/src/utils/DatabaseConnection.java
+++ UninaSwap/src/utils/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/UninaSwap";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Sonofigo88";
+    private static final String PASSWORD = "UninaSwap2526";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
